775ed445a107aef2b24de39c5908afe7b049bdda
jesiel364 <you@exampgit config --global user.email jesiel364@gmail.comle.com>
2022-03-12 11:04:57 -0400

    git token

---
+++ produtos/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from djmoney.models.fields import MoneyField
+
+class Base(models.Model):
+  criacao = models.DateTimeField(auto_now_add=True)
+  atualizacao = models.DateTimeField(auto_now=True)
+  ativo = models.BooleanField(default=True)
+  
+  class Meta:
+    abstract = True
+    
+class Produto(Base):
+  titulo = models.CharField(max_length=255)
+  marca = models.CharField(max_length=255)
+  preco = models.DecimalField(max_digits=5000, decimal_places=2)
+  
+  class Meta:
+    verbose_name = 'Produto'
+    verbose_name_plural = 'Produtos'
+    
+  def __str__(self):
+    return self.titulo
+
+class Estoque(Base):
+  produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+  codigo = models.CharField(max_length=8, default='123456am')
+  quantidade = models.IntegerField()
+  locacao = models.CharField(max_length=255, default='1A01', verbose_name='locação')
+  excesso = models.CharField(max_length=255, blank=True, default='')
+  
+  class Meta:
+    verbose_name = 'Estoque'
+    verbose_name_plural= 'Estoques'
+    
+  def __str__(self):
+    return f'{self.produto} esta com {self.quantidade} unidades nas locações {self.locacao} e excesso {self.excesso}'
