62910e68557c27e7d21ffc8a2674fde5a6a2ba08
jesiel364 <jesiel364@gmail.com>
2022-03-12 17:46:46 -0400

    Add bootstrap template, navbar, page home

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from produtos.views import ProdutoListView
+from produtos.views import ProdutoListView, EstoqueListView, home_view
+from produtos.models import Produto
+
+app_name = 'produtos'
+
+# PROCURANDO POR TITULO
+# res = []
+# for item in Produto.objects.filter(titulo__contains='125'):
+#   res.append(item.titulo)
+# print(res)
 
 urlpatterns = [
-  path('produtos/', ProdutoListView.as_view())
+  path('produtos/', ProdutoListView.as_view()),
+  path('estoques/', EstoqueListView.as_view()),
+  path('', home_view),
   ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from produtos.models import Produto
+from produtos.models import Produto, Estoque
 
 class ProdutoListView(ListView):
   model = Produto
+
+class EstoqueListView(ListView):
+  model = Estoque
+
+def home_view(request):
+  return render(request, 'home.html')
